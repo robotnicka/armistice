7216b47ab99b6d1f366068062c7ff0f07ae7dcce
robotnicka <worshipoldgods@gmail.com>
2018-10-08 18:22:41 -0400

    Images and includes, js updates

--- inc/customizer.php
+++ inc/customizer.php
@@ -10,6 +10,8 @@
  *
  * @param WP_Customize_Manager $wp_customize Theme Customizer object.
  */
+
+ // Implementing custom settings in WP Customize
 function armistice_customize_register( $wp_customize ) {
 	$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
 	$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
@@ -26,37 +28,95 @@ function armistice_customize_register( $wp_customize ) {
 		) );
 	}
 
-	$wp_customize->add_section( 'armistice_new_section_name' , array(
+	$wp_customize->add_section( 'armistice_date_and_location_info' , array(
 	    'title'      => __('Date and Location Info','mytheme'),
 	    'priority'   => 30,
 	) );
+	
+	$wp_customize->add_section( 'armistice_media_highlighter' , array(
+	    'title'      => __('Media Highlight','mytheme'),
+	    'priority'   => 30,
+	) );
 
-	$wp_customize->add_setting( 'armistice_date_text', array(
+	$wp_customize->add_setting( 'armistice_date_text_1', array(
 	  'capability' => 'edit_theme_options',
 	  'default' => 'e.g. Nov 15',
 	  'sanitize_callback' => 'sanitize_text_field',
 	) );
 
-	$wp_customize->add_control( 'armistice_date_text', array(
+	$wp_customize->add_control( 'armistice_date_text_1', array(
 	  'type' => 'text',
-	  'section' => 'armistice_new_section_name',
+	  'section' => 'armistice_date_and_location_info',
 	  'label' => __( 'Event Date' ),
 	  'description' => __( 'Displays date on home page.' ),
 	) );
 
-	$wp_customize->add_setting( 'armistice_location_text', array(
+	$wp_customize->add_setting( 'armistice_location_text_1', array(
 	  'capability' => 'edit_theme_options',
 	  'default' => 'e.g. New Orleans',
 	  'sanitize_callback' => 'sanitize_text_field',
 	) );
 
-	$wp_customize->add_control( 'armistice_location_text', array(
+	$wp_customize->add_control( 'armistice_location_text_1', array(
 	  'type' => 'text',
-	  'section' => 'armistice_new_section_name',
+	  'section' => 'armistice_date_and_location_info',
 	  'label' => __( 'Event Location' ),
 	  'description' => __( 'Displays event location on home page.' ),
 	) );
+	
+	$wp_customize->add_setting( 'armistice_date_text_2', array(
+	  'capability' => 'edit_theme_options',
+	  'default' => 'e.g. Nov 15',
+	  'sanitize_callback' => 'sanitize_text_field',
+	) );
+
+	$wp_customize->add_control( 'armistice_date_text_2', array(
+	  'type' => 'text',
+	  'section' => 'armistice_date_and_location_info',
+	  'label' => __( 'Event Date' ),
+	  'description' => __( 'Displays date on home page.' ),
+	) );
+
+	$wp_customize->add_setting( 'armistice_location_text_2', array(
+	  'capability' => 'edit_theme_options',
+	  'default' => 'e.g. New Orleans',
+	  'sanitize_callback' => 'sanitize_text_field',
+	) );
+
+	$wp_customize->add_control( 'armistice_location_text_2', array(
+	  'type' => 'text',
+	  'section' => 'armistice_date_and_location_info',
+	  'label' => __( 'Event Location' ),
+	  'description' => __( 'Displays event location on home page.' ),
+	) );
+	
+	$wp_customize->add_setting( 'armistice_media_highlight', array(
+	  'capability' => 'edit_theme_options',
+	  'default' => '',
+	  'sanitize_callback' => 'sanitize_text_field',
+	) );
+
+	$wp_customize->add_control( 'armistice_media_highlight', array(
+	  'type' => 'text',
+	  'section' => 'armistice_media_highlighter',
+	  'label' => __( 'Media Highlight' ),
+	  'description' => __( 'Displays a highlighted piece of text on home page.' ),
+	) );
+	
+	$wp_customize->add_setting( 'armistice_media_highlight_link', array(
+	  'capability' => 'edit_theme_options',
+	  'default' => '',
+	  'sanitize_callback' => 'sanitize_text_field',
+	) );
+
+	$wp_customize->add_control( 'armistice_media_highlight_link', array(
+	  'type' => 'text',
+	  'section' => 'armistice_media_highlighter',
+	  'label' => __( 'Media Highlight Link' ),
+	  'description' => __( 'Displays a highlighted piece of text on home page.' ),
+	) );
 }
+
 add_action( 'customize_register', 'armistice_customize_register' );
 
 /**
